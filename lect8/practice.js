cfa6ebf38bba6670a987687df5a683a4abff6c79
shubham0419 <shubhamsc704707@gmail.com>
2025-06-20 14:40:09 +0530

    async JS

--- lect8/practice.js
+++ lect8/practice.js
@@ -6,10 +6,41 @@ let menu = [
 
 // filter out veg and non-veg items.
 
-let produts = [
+let products = [
   {
-    name:"product 1 name",
-    category:"wearable",
-    color:["red","white"]
+    name: "product 1 name",
+    category: "wearable",
+    color: ["red", "white"]
+  },
+  {
+    name: "product 2 name",
+    category: "playable",
+    color: ["black", "silver"]
+  },
+  {
+    name: "product 3 name",
+    category: "wearable",
+    color: ["blue", "white"]
+  },
+  {
+    name: "product 4 name",
+    category: "playable",
+    color: ["white", "gray"]
+  },
+  {
+    name: "product 5 name",
+    category: "playable",
+    color: ["gold", "black"]
+  },
+  {
+    name: "product 6 name",
+    category: "wearable",
+    color: ["orange", "black"]
+  },
+  {
+    name: "product 7 name",
+    category: "wearable",
+    color: ["purple", "pink"]
   }
-]
+];
+

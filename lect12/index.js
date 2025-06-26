8187ed447027ef27f60e6256be822ffae4632689
shubham0419 <shubhamsc704707@gmail.com>
2025-06-26 15:59:24 +0530

    todo

--- lect12/index.js
+++ lect12/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 const PORT = 4444;
 
 app.use(express.json()); // axios sends the json data
@@ -9,15 +10,44 @@ app.use(express.static(path.join(__dirname, 'public'))); // public folder ko bhe
 
 let Todos = [
     // {
+    //     id:"";
     //     task:"something",
     //     completed:false
     // }
 ];
-
 app.post("/add-todo",(req,res)=>{
     const task = req.body.task;
-    Todos.push({task:task,completed:false});
+    Todos.push({task:task,completed:false,id:uuidv4()});
     res.status(201).json({Todos});
+})
+
+app.delete("/todo/:id",(req,res)=>{
+    const id = req.params.id;
+    Todos = Todos.filter((todo)=>{
+        return todo.id != id;
+    })
+    res.status(200).json({Todos});
+})
+
+app.put("/todo/:id",(req,res)=>{
+    const id = req.params.id;
+    Todos = Todos.map((todo)=>{
+        if(todo.id == id){
+            return {
+                ...todo,
+                completed:!(todo.completed)
+            }
+        }
+        return todo;
+    })
+    res.status(200).json({Todos});
+})
+
+app.delete("/clear-complete",(req,res)=>{
+    Todos = Todos.filter((todo)=>{
+        return todo.completed==false;
+    })
+    res.status(200).json({Todos});
 })
 
 app.get("/all-todos",(req,res)=>{
--- lect12/public/script.js
+++ lect12/public/script.js
@@ -1,15 +1,36 @@
 const btnContainer = document.getElementById("btn-container")
 const form = document.getElementById("todo-form");
 const container = document.getElementById("container");
+const clearBtn = document.getElementById("clear");
+
+clearBtn.addEventListener("click",async ()=>{
+    let res = await axios.delete("http://localhost:4444/clear-complete");
+    renderTodos(res.data.Todos);
+})
+
+container.addEventListener("click",async (e)=>{
+    if(e.target.id=="delete"){
+        const id = e.target.parentElement.id;
+        const res = await axios.delete(`http://localhost:4444/todo/${id}`)
+        renderTodos(res.data.Todos)
+    }
+    if(e.target.id=="complete"){
+        const id = e.target.parentElement.id;
+        const res = await axios.put(`http://localhost:4444/todo/${id}`)
+        renderTodos(res.data.Todos)
+    }
+})
 
 form.addEventListener("submit",async (e)=>{
     e.preventDefault();
     const taskText = e.target.children[0].value;
     if(taskText.trim().length==0){
         alert("Please enter valid task");
+        return;
     }
 
     let result = await axios.post("http://localhost:4444/add-todo",{task:taskText});
+    e.target.children[0].value = "";
     renderTodos(result.data.Todos);
 })
 
@@ -18,8 +39,10 @@ function renderTodos(todos){
     for(let todo of todos){
         let div = document.createElement("div");
         div.className = "flex w-full justify-between border p-2 rounded-md"
-        div.innerHTML = `${todo.task} <div class="flex gap-2">
-            <button id="complete" class="bg-green-500 px-3 py-1">Completed</button>
+        div.innerHTML = `${todo.task} <div id=${todo.id} class="flex gap-2">
+            <button id="complete" class="bg-green-500 px-3 py-1">
+            ${todo.completed?"Undo":"Complete"}
+            </button>
             <button id="delete" class="bg-red-500 px-3 py-1">Delete</button>
         </div>`
         container.append(div);

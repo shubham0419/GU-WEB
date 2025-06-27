fbec4f9fb3d379330c1988166e6ee2546bc5f205
shubham0419 <shubhamsc704707@gmail.com>
2025-06-27 12:55:28 +0530

    db

--- lect13/index.js
+++ lect13/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 require('dotenv').config()
 const PORT = 4000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 
-console.log(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
+  console.log("DB connected");
+}).catch((error)=>{
+  console.log(error);
+})
+
 
 app.listen(PORT,()=>{
   console.log(`live on ${PORT}`);
